26858c5181f545f7c9d46742cdd54b464e8ed9d2
hjo3736 <aria3736@gm.gist.ac.kr>
2022-07-28 15:29:01 +0900

    Item description

--- Assets/Scripts/ui/EquipSlot.cs
+++ Assets/Scripts/ui/EquipSlot.cs
@@ -43,6 +43,37 @@ public class EquipSlot : MonoBehaviour, IPointerClickHandler
             item_info.GetComponentsInChildren<Image>()[2].sprite = _item.item_image;
             item_info.GetComponentsInChildren<TMP_Text>()[0].text = _item.item_name.ToString();
 
+            switch (_item.item_name)
+            {
+                case ITEM_NAME.SWORD_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "Attak_pt + 5";
+                    break;
+                case ITEM_NAME.AX_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "Attak_pt + 7";
+                    break;
+                case ITEM_NAME.ARMOR_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "Def + 5";
+                    break;
+                case ITEM_NAME.ARMOR_02:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "Def + 10";
+                    break;
+                case ITEM_NAME.ARTIFACT_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "HP + 1 EVERY TURN";
+                    break;
+                case ITEM_NAME.FOOD_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "HP + 5\nHunger + 300";
+                    break;
+                case ITEM_NAME.POTION_HP:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "HP + 30";
+                    break;
+                case ITEM_NAME.RING_01:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "Dodge + 10";
+                    break;
+                case ITEM_NAME.RING_02:
+                    item_info.GetComponentsInChildren<TMP_Text>()[1].text = "MaxHP + 30";
+                    break;
+            }
+
             item_info.GetComponentsInChildren<TMP_Text>()[2].text = "UNEQUIP";
 
         }
